6f5ffecd34021bfd88fd3752d64a1795bb3bc8ad
RichardT <chardiecode@gmail.com>
2023-03-28 17:05:04 +0800

    Add email auth provider

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -8,6 +8,7 @@ import DiscordProvider from "next-auth/providers/discord";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { env } from "~/env.mjs";
 import { prisma } from "~/server/db";
+import EmailProvider from "next-auth/providers/email";
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
@@ -47,10 +48,22 @@ export const authOptions: NextAuthOptions = {
   },
   adapter: PrismaAdapter(prisma),
   providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
-    }),
+    EmailProvider({
+      server: {
+        host: process.env.EMAIL_SERVER || 'https://localhost:3000',
+        port: 587,
+        auth: {
+          user: 'apikey',
+          pass: process.env.EMAIL_PASSWORD || ''
+        }
+      },
+      from: process.env.EMAIL_FROM || 'default@default.com',
+      ...(process.env.NODE_ENV !== 'production' ? {
+        sendVerificationRequest({url}) {
+          console.log('LOGIN LINK', url);
+        }
+      } : {}),
+    })
     /**
      * ...add more providers here.
      *
